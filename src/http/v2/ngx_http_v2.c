f62861e61368cc77775667e96cd3d39804cb4ffc
Cory Forsyth <cory.forsyth@addepar.com>
2022-02-22 16:30:52 +0100

    more debugging

--- src/http/v2/ngx_http_v2.c
+++ src/http/v2/ngx_http_v2.c
@@ -1310,8 +1310,8 @@ ngx_http_v2_state_headers(ngx_http_v2_connection_t *h2c, u_char *pos,
     h2c->state.header_limit = cscf->large_client_header_buffers.size
                               * cscf->large_client_header_buffers.num;
 
-    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, h2c->connection->log, 0,
-		    "http2-extra: state.header_limit: %i, large_client_header_buffers.num: %i", h2c->state.header_limit, cscf->large_client_header_buffers.num);
+    ngx_log_debug3(NGX_LOG_DEBUG_HTTP, h2c->connection->log, 0,
+		    "http2-extra: state.header_limit: %i, large_client_header_buffers.num: %i, .size %i", h2c->state.header_limit, cscf->large_client_header_buffers.num, cscf->large_client_header_buffers.size);
 
     h2scf = ngx_http_get_module_srv_conf(h2c->http_connection->conf_ctx,
                                          ngx_http_v2_module);
@@ -1768,8 +1768,8 @@ ngx_http_v2_state_process_header(ngx_http_v2_connection_t *h2c, u_char *pos,
     }
 
     len = header->name.len + header->value.len;
-    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, h2c->connection->log, 0,
-		    "http2-extra: header name.len: %i  value.len: %i", header->name.len, header->value.len);
+    ngx_log_debug4(NGX_LOG_DEBUG_HTTP, h2c->connection->log, 0,
+		    "http2-extra: header name \"%V\" name.len: %i, value.len: %i, total len: %i", &header->name, header->name.len, header->value.len, len);
 
     if (len > h2c->state.header_limit) {
         ngx_log_error(NGX_LOG_INFO, h2c->connection->log, 0,
@@ -1780,7 +1780,7 @@ ngx_http_v2_state_process_header(ngx_http_v2_connection_t *h2c, u_char *pos,
 
     h2c->state.header_limit -= len;
     ngx_log_debug2(NGX_LOG_DEBUG_HTTP, h2c->connection->log, 0,
-		    "http2-extra: header_limit reduced by %i to %i", len, h2c->state.header_limit);
+		    "http2-extra: header_limit reduced by %i bytes to %i bytes", len, h2c->state.header_limit);
 
     if (h2c->state.index) {
         if (ngx_http_v2_add_header(h2c, header) != NGX_OK) {
